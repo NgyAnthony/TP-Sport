3662ebed60fb446cd88bf690b61baa9c2db2c375
Anthony NGUYEN <anthony.ng@hotmail.fr>
2020-02-27 11:12:04 +0100

    Push data test

--- src/app/data/sports.ts
+++ src/app/data/sports.ts
@@ -4,5 +4,11 @@ export const SPORTS = [
         title: 'Football',
         description: 'Soccer is one of the biggest top 10 sports in the world. ',
         mainImageLink: '/football.jpg',
-    }
+    },
+    {
+        id: 2,
+        title: 'Basketball',
+        description: 'Basketball is one of the biggest top 10 sports in the world. ',
+        mainImageLink: '/football.jpg',
+    },
 ];
