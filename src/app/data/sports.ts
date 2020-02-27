f4d038863067f5805515c5cb57ea6a411a615465
Anthony NGUYEN <anthony.ng@hotmail.fr>
2020-02-27 11:31:41 +0100

    Add data

--- src/app/data/sports.ts
+++ src/app/data/sports.ts
@@ -2,25 +2,71 @@ export const SPORTS = [
     {
         id: 1,
         title: 'Football',
-        description: 'Soccer is one of the biggest top 10 sports in the world. ',
+        description: 'Soccer is one of the biggest top 10 sports in the world.',
         mainImageLink: '/football.jpg',
+        videoLink: 'https://youtu.be/QRtJnFvKimY'
     },
     {
         id: 2,
-        title: 'Basketball',
-        description: 'Basketball is one of the biggest top 10 sports in the world. ',
-        mainImageLink: '/football.jpg',
+        title: 'Cricket',
+        description: 'Cricket is one of the most popular and top 10 sports in the world. ',
+        mainImageLink: '/cricket.jpg',
+        videoLink: 'https://youtu.be/Kwu1yIC-ssg'
     },
     {
         id: 3,
-        title: 'Cricket',
-        description: 'Basketball is one of the biggest top 10 sports in the world. ',
-        mainImageLink: '/football.jpg',
+        title: 'Hockey',
+        description: 'Field Hockey is one among the top 10 sports in the world.',
+        mainImageLink: '/hockey.jpg',
+        videoLink: 'https://youtu.be/05i6Ij2OxRA'
     },
     {
         id: 4,
-        title: 'Baseball',
-        description: 'Basketball is one of the biggest top 10 sports in the world. ',
-        mainImageLink: '/football.jpg',
+        title: 'Tennis',
+        description: 'Tennis is one of the longest standing and most reputable, top 10 sports in the world',
+        mainImageLink: '/tennis.jpg',
+        videoLink: 'https://youtu.be/eCwPpAApzDQ'
     },
+    {
+        id: 5,
+        title: 'Volley',
+        description: 'Volleyball is a simple game and is most popular sport in the world.',
+        mainImageLink: '/volley.jpg',
+        videoLink: 'https://youtu.be/TioYGmL-HRQ'
+    },
+    {
+        id: 6,
+        title: 'Ping Pong',
+        description: 'Ping Pong is a racquet sport that pits two or four players against each other around a table. Table tennis is a leisure activity, but it has also been an Olympic sport since 1988.',
+        mainImageLink: '/pingpong.jpg',
+        videoLink: 'https://youtu.be/A9wTx2Pq6L4'
+    },
+    {
+        id: 7,
+        title: 'Basketball',
+        description: 'Basketball or basketball, often referred to in French by its abbreviation basket, is a team sport between two teams of five players on a rectangular court.',
+        mainImageLink: '/basketball.jpg',
+        videoLink: 'https://youtu.be/xjPi6IcSH_Q'
+    },
+    {
+        id: 8,
+        title: 'Baseball',
+        description: 'Baseball is a team sport derived from the same roots as cricket, which is played on a grass and sand pitch. It is played with bats to hit a pitched ball, and gloves to catch the ball.',
+        mainImageLink: '/baseball.jpg',
+        videoLink: 'https://youtu.be/yROjP3a6NSs'
+    },
+    {
+        id: 9,
+        title: 'Golf',
+        description: 'Golf is a precision sport played outdoors, where clubs are used to hit a ball into a hole. The aim of the game is to make as few strokes as possible on a defined course. Precision, endurance, technicality, concentration are essential qualities for this activity.',
+        mainImageLink: '/golf.jpg',
+        videoLink: 'https://youtu.be/7norsLxN8ko'
+    },
+    {
+        id: 10,
+        title: 'Rugby',
+        description: 'Rugby Union Rugby, also known in English-speaking countries as Rugby Union Rugby, which is played by teams of fifteen players on the field with substitutes, is the most popular variation of rugby, a family of team sports, whose specific features are scrums and touches, pitting two teams against each other as they compete for an oval ball, played by hand and foot.',
+        mainImageLink: '/rugby.jpg',
+        videoLink: 'https://youtu.be/fV-IwG9lNEY'
+    }
 ];
