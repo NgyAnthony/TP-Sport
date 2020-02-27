8db39baa21406ba240583814fdf9ebad5584ef4b
Anthony NGUYEN <anthony.ng@hotmail.fr>
2020-02-27 11:58:04 +0100

    Add sport component

---
+++ src/app/sport-details/sport-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sport-details',
+  templateUrl: './sport-details.component.html',
+  styleUrls: ['./sport-details.component.scss']
+})
+export class SportDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
