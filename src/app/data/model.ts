4298436e350385dd4ce631a2f6da8678c72fa78b
Anthony NGUYEN <anthony.ng@hotmail.fr>
2020-02-27 11:09:11 +0100

    Adjust model

--- src/app/data/model.ts
+++ src/app/data/model.ts
@@ -2,9 +2,7 @@ export class Model {
     id: number;
     title: string;
     description?: string;
-    details?: string;
     videoLink?: string;
     mainImageLink?: string;
     detail1ImageLink?: string;
-    detail2ImageLink?: string;
 }
--- src/app/data/sports.ts
+++ src/app/data/sports.ts
@@ -3,13 +3,6 @@ export const SPORTS = [
         id: 1,
         title: 'Football',
         description: 'Soccer is one of the biggest top 10 sports in the world. ',
-        details: ' Soccer is one of the biggest top 10 sports in the world. This is because it has the most followers who play and watch ' +
-            'the game. Soccer has the largest fan following in the world and undoubtedly, everyone loves the beautiful game.\n' +
-            '\n' +
-            'According to the Bleacher Report on the Most Popular Sports, soccer is played in 208 countries and its fan following is ' +
-            'first in 93 countries with a combine population of 2 billion. It is in the top 3 sports in 100 countries with ' +
-            '3 billion people. The soccer World Cup is the largest and most lucrative sporting event; Soccer is a team ' +
-            'sport so that as many as 22 people can play in one game which helps to promote team spirit.',
-        mainImageLink: '/football.jpg'
+        mainImageLink: '/football.jpg',
     }
 ];
