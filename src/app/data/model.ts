842bb81226b246247692340ca32d49012a8de409
Anthony NGUYEN <anthony.ng@hotmail.fr>
2020-02-27 10:28:10 +0100

    Add model and data

---
+++ src/app/data/model.ts
@@ -0,0 +1,10 @@
+export class Model {
+    id: number;
+    title: string;
+    description?: string;
+    details?: string;
+    videoLink?: string;
+    mainImageLink?: string;
+    detail1ImageLink?: string;
+    detail2ImageLink?: string;
+}
---
+++ src/app/data/sports.ts
@@ -0,0 +1,6 @@
+export const SPORTS = [
+    {
+        id: 1,
+        title: 'Football'
+    }
+];
